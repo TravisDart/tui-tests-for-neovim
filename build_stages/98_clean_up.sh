8895dfe48b2a6f2ccfde8d2a62969671e98626fe
Travis Dart <travis.dart@connectwise.com>
2025-10-17 16:03:58 -0400

    Cleanup changes

--- build_stages/98_clean_up.sh
+++ build_stages/98_clean_up.sh
@@ -3,13 +3,15 @@ source "$(dirname "$0")/00_env.sh"
 
 echo "Cleaning up... (Ignore any errors.)"
 
-docker stop $(docker ps | grep $DOCKER_PREFIX | awk '{print $1}') 2>/dev/null > /dev/null
-docker rm $(docker ps -a | grep $DOCKER_PREFIX | awk '{print $1}') 2>/dev/null > /dev/null
+docker stop $(docker ps | grep $LOCAL_IMAGE_NAME | awk '{print $1}') 2>/dev/null > /dev/null
 
 for PYTHON_VERSION in "${PYTHON_VERSIONS[@]}"; do
+  docker image rm "$LOCAL_IMAGE_NAME:$PYTHON_VERSION" 2>/dev/null > /dev/null
   docker image rm "$PUBLISHED_IMAGE_PREFIX:$PYTHON_VERSION" 2>/dev/null > /dev/null
 done
+docker image rm "$LOCAL_IMAGE_NAME:latest" 2>/dev/null > /dev/null
 docker image rm "$PUBLISHED_IMAGE_PREFIX:latest" 2>/dev/null > /dev/null
+docker image rm "$PYTEST_CONTAINER_NAME" 2>/dev/null > /dev/null
+docker image rm "$ADVANCED_TEST_CONTAINER_PREFIX" 2>/dev/null > /dev/null
 
-docker image rm $(docker images | grep $DOCKER_PREFIX | awk '{print $3}') 2>/dev/null > /dev/null
 docker volume rm $WORKSPACE_VOLUME_NAME 2>/dev/null > /dev/null
